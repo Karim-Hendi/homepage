2d39abb0e4275199329d7dd180a80cf9c35b82c6
Your Name <kareemhinde8@gmail.com>
2025-05-01 20:56:09 +0200

    Home-Page

---
+++ src/component/Found/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function found() {
+  return (
+    <div>
+      
+      <h1>Found Component</h1>
+    </div>
+  )
+}
+
+export default found
---
+++ src/component/Lost/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Lost() {
+  return (
+    <div>
+      
+      <h1>Lost Component</h1>
+    </div>
+  )
+}
+
+export default Lost
